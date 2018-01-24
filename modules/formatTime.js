a7dc6aa773fe5e654ffc9c1c6858552065a0521f
Piotr Sociński <psocinski@gmail.com>
2018-01-24 20:32:00 +0100

    Finieshed

--- modules/formatTime.js
+++ modules/formatTime.js
@@ -5,13 +5,19 @@ function formatTime(seconds) {
     	return (e < 10 ? '0' : '') + e;
   
   	}
-  
+
+  	function formatDay(d) {
+
+  		return (d != 1 ? 's' : '');
+
+  	}
+  	
 	var days = Math.floor(seconds / (3600*24));  
   	var hours = Math.floor(seconds % (3600*24) / 3600);
   	var minutes = Math.floor(seconds % (60*60) / 60);
   	var seconds = Math.floor(seconds % 60);
 
-  	return addZero(days) + ' day ' + addZero(hours) + ' hr ' + addZero(minutes) + ' min ' + addZero(seconds) + ' sec';
+  	return addZero(days) + ' day' + formatDay(days) + ' ' + addZero(hours) + ' hr ' + addZero(minutes) + ' min ' + addZero(seconds) + ' sec';
 
 }
 
